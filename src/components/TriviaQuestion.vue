0322b13218f5f57f20af153862ee70a6dd072324
Felipe Flor <f.flor@vehikl.com>
2019-10-22 21:50:19 -0400

    Render one button for each possible answer

--- src/components/TriviaQuestion.vue
+++ src/components/TriviaQuestion.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="trivia-question">
         <div class="question-prompt" v-text="trivia.question"/>
+        <button class="possible-answer"
+                v-for="(answer, index) in possibleAnswers"
+                :key="index"
+                v-text="answer"
+                :content="answer"/>
     </div>
 </template>
 
@@ -12,5 +17,12 @@
     export default class TriviaQuestion extends Vue {
         @Prop({required: true}) private trivia!: ITrivia;
 
+        private get possibleAnswers(): string[] {
+            const possibleAnswers = [...this.trivia.incorrect_answers];
+            const indexOfRightAnswer: number = Math.floor(Math.random() * possibleAnswers.length);
+            possibleAnswers.splice(indexOfRightAnswer, 0, this.trivia.correct_answer);
+
+            return possibleAnswers;
+        }
     }
 </script>
--- tests/unit/TriviaQuestion.spec.ts
+++ tests/unit/TriviaQuestion.spec.ts
@@ -2,27 +2,41 @@ import {shallowMount, Wrapper} from "@vue/test-utils";
 import TriviaQuestion from "@/components/TriviaQuestion.vue";
 import {ITrivia} from "@/types";
 
-describe("TriviaQuestion", () => {
-    it("Renders a given question", () => {
-        const trivia: ITrivia = {
-            category: "Movies",
-            correct_answer: "Movie that is correct",
-            difficulty: "Easy",
-            incorrect_answers: [
-                "Wrong Movie 1",
-                "Wrong Movie 2",
-                "Wrong Movie 3",
-            ],
-            question: "What is the right movie?",
-            type: "Multiple-choice",
-        };
+const trivia: ITrivia = {
+    category: "Movies",
+    correct_answer: "Movie that is correct",
+    difficulty: "Easy",
+    incorrect_answers: [
+        "Wrong Movie 1",
+        "Wrong Movie 2",
+        "Wrong Movie 3",
+    ],
+    question: "What is the right movie?",
+    type: "Multiple-choice",
+};
 
-        const wrapper: Wrapper<TriviaQuestion> = shallowMount(TriviaQuestion, {
+const possibleAnswers: string[] = [...trivia.incorrect_answers, trivia.correct_answer];
+
+describe("TriviaQuestion", () => {
+    let wrapper: Wrapper<TriviaQuestion>;
+
+    beforeEach(() => {
+        wrapper = shallowMount(TriviaQuestion, {
             propsData: {
                 trivia,
             },
         });
+    });
 
+    it("Renders a given question", () => {
         expect(wrapper.find(".question-prompt").text()).toEqual(trivia.question);
+    });
+
+    it("Renders one button for each possible answer of a given question", () => {
+        expect(wrapper.findAll(".possible-answer")).toHaveLength(possibleAnswers.length);
+
+        possibleAnswers.forEach((answer: string) => {
+            expect(wrapper.find(`.possible-answer[content="${answer}"]`).exists()).toBe(true);
+        });
     });
 });
