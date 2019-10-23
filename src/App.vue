f93b545d5f72644334b9a34fdf44cd80a19d15e5
Felipe Flor <f.flor@vehikl.com>
2019-10-22 22:05:54 -0400

    Use fake question data in app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <div id="app">
-  </div>
+    <div id="app">
+        <trivia-question :trivia="question"/>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+    import {Component, Vue} from "vue-property-decorator";
+    import TriviaQuestion from "@/components/TriviaQuestion.vue";
+    import {ITrivia} from "@/types";
 
-@Component({
-  components: {
-  },
-})
-export default class App extends Vue {}
+    @Component({
+        components: {
+            TriviaQuestion
+        },
+    })
+    export default class App extends Vue {
+        private question: ITrivia = {
+            category: "Movies",
+            correct_answer: "Movie that is correct",
+            difficulty: "Easy",
+            incorrect_answers: [
+                "Wrong Movie 1",
+                "Wrong Movie 2",
+                "Wrong Movie 3",
+            ],
+            question: "What is the right movie?",
+            type: "Multiple-choice",
+        };
+    }
 </script>
 
 <style lang="scss">
+    #app {
+
+    }
 </style>
